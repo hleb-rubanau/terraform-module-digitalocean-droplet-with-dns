076aeb85ba27b30415c26732a3fe25806099f00c
Hleb Rubanau <g.rubanau@gmail.com>
2020-06-10 14:17:47 +0300

    add FQDN to the module outputs

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,3 @@
 output "droplet" { value = digitalocean_droplet.server.name }
 output "ip_address" { value = digitalocean_droplet.server.ipv4_address }
+output "domain" { value = digitalocean_record.dns.fqdn }
